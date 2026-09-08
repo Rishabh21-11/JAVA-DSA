5ad6c66a8582bac14874d90d743a7ac59bc77c66
rishabh21-11 <rishabhm2111@gmail.com>
2026-09-08 19:19:32 +0530

    Initial commit

--- day6.java
+++ day6.java
@@ -42,7 +42,7 @@ public class day6{
         for(int i=0;i<arr.length;i++){
             int min = i;
             for(int j=i+1;j<arr.length;j++){
-                if(arr[j] <arr[min]){
+                if(arr[j] >arr[min]){
                     min = j;
                 }
             }
