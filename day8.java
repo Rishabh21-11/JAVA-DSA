eb826a6747e641da14cb77891ff580981af8c3d4
rishabh21-11 <rishabhm2111@gmail.com>
2026-09-10 13:08:05 +0530

    new commit

--- day8.java
+++ day8.java
@@ -30,15 +30,24 @@ public class day8 {
     //         System.out.println();
     //     }
 
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=n;j++){
-                System.out.print("*");
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=n;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
             }
             System.out.println();
         }
+
+
 
     }
 
